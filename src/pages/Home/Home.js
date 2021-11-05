bd1cde2517ed32554acde0a87893febae53584c1
SMNahuel <nahuelsan96@gmail.com>
2021-11-05 18:29:54 -0300

    responsive/iPhone

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,29 +3,28 @@ import Navbar from "../../components/Navbar";
 import style from "./Home.module.css";
 
 const Home = () => {
+
   return (
-    <>
-      <div className={style.Container}>
-        <Navbar />
-        <div className={style.ContainerAbout}>
-          <div className={style.Left}>
-            <p>SO, YOU WANT TO TRAVEL TO</p>
-            <h1>SPACE</h1>
-            <p>
-              Let’s face it; if you want to go to space, you might as well
-              genuinely go to outer space and not hover kind of on the edge of
-              it. Well sit back, and relax because we’ll give you a truly out of
-              this world experience!
-            </p>
-          </div>
-          <div className={style.EffectAmazing}>
-            <div className={style.Rigth}>
-              <p>EXPLORE</p>
-            </div>
+    <div className={style.Container}>
+      <Navbar />
+      <div className={style.ContainerAbout}>
+        <div className={style.Left}>
+          <p>SO, YOU WANT TO TRAVEL TO</p>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className={style.EffectAmazing}>
+          <div className={style.Rigth}>
+            <p>EXPLORE</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
